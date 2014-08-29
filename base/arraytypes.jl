c79014ef579cf49032f9ef0437106b42725cd40b
jake bolewski <jakebolewski@gmail.com>
2014-08-28 22:53:47 -0400

    move typealiases to arraytypes.jl

--- base/arraytypes.jl
+++ base/arraytypes.jl
@@ -1,3 +1,12 @@
+typealias Vector{T} Array{T,1}
+typealias Matrix{T} Array{T,2}
+typealias VecOrMat{T} Union(Vector{T}, Matrix{T})
+
+typealias DenseVector{T} DenseArray{T,1}
+typealias DenseMatrix{T} DenseArray{T,2}
+typealias DenseVecOrMat{T} Union(DenseVector{T}, DenseMatrix{T})
+
+
 typealias StridedArray{T,N,A<:DenseArray} Union(DenseArray{T,N}, SubArray{T,N,A})
 typealias StridedVector{T,A<:DenseArray}  Union(DenseArray{T,1}, SubArray{T,1,A})
 typealias StridedMatrix{T,A<:DenseArray}  Union(DenseArray{T,2}, SubArray{T,2,A})
